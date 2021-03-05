3648370cef12d63cca7edb861c84080ebc685f9f
mehrabix <mehrabi@post.com>
2021-03-06 00:05:49 +0330

    dd

--- src/App.js
+++ src/App.js
@@ -1,35 +1,33 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { CardList } from "./components/card-list/card-list.component";
+import "./App.css";
 
-class App extends React.Component{
+class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-       
-      ]
-      
+      monsters: [],
     };
   }
 
-// life cycle method
+  // life cycle method
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ monsters: users})); 
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
 
-  render(){
-  return (
-    <div className='App'>
-      {
-        this.state.monsters.map(monsters => (
-          <h1 key={monsters.id}> {monsters.name}</h1>
-        ))
-      }
-    </div>
-  )
-}
+  render() {
+    return (
+      <div className="App">
+        <CardList name="ahmad">
+          {this.state.monsters.map((monsters) => (
+            <h1 key={monsters.id}> {monsters.name}</h1>
+          ))}
+        </CardList>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,3 +1,6 @@
 import React from 'react';
+import './card-list.styles.css';
 
-export const CardList = () => <div></div>
+export const CardList = props => {
+    return <div className='card-list'>{props.children}</div>;
+};
