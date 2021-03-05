1168d6cf4564b23b0a8921174f308c76844e0434
mehrabix <mehrabi@post.com>
2021-03-05 17:54:51 +0330

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+class App extends React.Component{
+  constructor() {
+    super();
+    this.state = {
+      monsters: [
+       
+      ]
+      
+    };
+  }
+
+// life cycle method
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users})); 
+  }
+
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {
+        this.state.monsters.map(monsters => (
+          <h1 key={monsters.id}> {monsters.name}</h1>
+        ))
+      }
     </div>
-  );
+  )
+}
 }
 
 export default App;
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,1 @@
+impoert React from 'react';
