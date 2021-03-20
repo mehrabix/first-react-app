8501d8d0fe53bfc8d72b2615edd2e7e21796e020
mehrabix <mehrabi@post.com>
2021-03-20 18:48:03 +0330

    add interactive search

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
     super();
     this.state = {
       monsters: [],
-      searchField: ''
+      searchField: "",
     };
   }
 
@@ -19,12 +19,19 @@ class App extends React.Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
-      <div className='App'>
-        <input type='search' placeholder='search monsters...' onChange={e =>
-          this.setState({ searchField: e.target.value })}
-         />
-        <CardList monsters={this.state.monsters} />
+      <div className="App">
+        <input
+          type="search"
+          placeholder="search monsters..."
+          onChange={(e) => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
